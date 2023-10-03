daa2b8d0d5c36860a33074778d16d376a7c6c773
joyndev <ahmedbutt@joyngroup.us>
2023-10-03 15:36:08 +0500

    views for user created

--- account_management/views.py
+++ account_management/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import *
+from utils.response import *
+
+
+class UserViews(viewsets.ModelViewSet):
+    def create(self, request, *args, **kwargs):
+        serialized_data = UserSerializer(data=request.data)
+        if serialized_data.is_valid():
+            serialized_data.save()
+            return ok(serialized_data.data)
