2e9e18f9e609fcc29b632e41f63abe3f950d9c92
joyndev <ahmedbutt@joyngroup.us>
2023-10-03 15:33:30 +0500

    allowed host set to all

--- new_django_project/settings.py
+++ new_django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xw&4iq*7!y)pa!sgbr2_2!6-o(t8a5i%_)_y=6s+t&8r#m(_r*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
