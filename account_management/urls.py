5431feb5eed1c28c76ffb070599b8332fb186264
joyndev <ahmedbutt@joyngroup.us>
2023-10-04 12:20:20 +0500

    signup url added

--- account_management/urls.py
+++ account_management/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from account_management.views import *
 
 urlpatterns = [
+    path('signup/', SignUpViews.as_view({'post': 'create'})),
     path('signin/', SignInViews.as_view({'post': 'create'})),
 ]
