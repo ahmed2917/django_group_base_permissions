690c8c8676a32e119748eaacd7fa09f24b05e496
joyndev <ahmedbutt@joyngroup.us>
2023-10-04 11:47:10 +0500

    signup view added  to add users

--- account_management/views.py
+++ account_management/views.py
@@ -10,5 +10,7 @@ class SignUpViews(viewsets.ModelViewSet):
     def create(self, request, *args, **kwargs):
         serialized_data = UserSerializer(data=request.data)
         if serialized_data.is_valid():
-            serialized_data.save()
-            return ok(serialized_data.data)
+            instance = serialized_data.save()
+            return ok(data=serialized_data.data)
+
+
