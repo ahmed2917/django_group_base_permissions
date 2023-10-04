6586d419ecae93de6f247e6783202aa79afb0a23
joyndev <ahmedbutt@joyngroup.us>
2023-10-04 12:07:42 +0500

    serializer error msg retrieving

--- utils/common_functions.py
+++ utils/common_functions.py
@@ -1,3 +1,25 @@
 from rest_framework.utils.serializer_helpers import ReturnList
 
 
+def get_first_error_message_from_serializer_errors(serialized_errors, default_message=""):
+    if not serialized_errors:
+        return default_message
+    try:
+
+        serialized_error_dict = serialized_errors
+
+        # ReturnList of serialized_errors when many=True on serializer
+        if isinstance(serialized_errors, ReturnList):
+            serialized_error_dict = serialized_errors[0]
+
+        serialized_errors_keys = list(serialized_error_dict.keys())
+        # getting first error message from serializer errors
+        try:
+            message = serialized_error_dict[serialized_errors_keys[0]][0].replace("This", serialized_errors_keys[0])
+            return message
+        except:
+            return serialized_error_dict[serialized_errors_keys[0]][0]
+
+    except Exception as e:
+        # logger.error(f"Error parsing serializer errors:{e}")
+        return default_message
