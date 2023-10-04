a43fd07405aac86162e88901a7bf97ea989b3c99
joyndev <ahmedbutt@joyngroup.us>
2023-10-04 11:49:26 +0500

    try catch added

--- account_management/views.py
+++ account_management/views.py
@@ -9,13 +9,17 @@ from utils.common_functions import *
 
 class SignUpViews(viewsets.ModelViewSet):
     def create(self, request, *args, **kwargs):
-        serialized_data = UserSerializer(data=request.data)
-        if serialized_data.is_valid():
-            instance = serialized_data.save()
-            instance.is_active = False
-            instance.save()
-            return ok(data=serialized_data.data)
-        else:
-            bad_request_msg = get_first_error_message_from_serializer_errors(serialized_data.errors,
-                                                                                'Bad request')
-            return bad_request(data=[], message=bad_request_msg)
+        try:
+            serialized_data = UserSerializer(data=request.data)
+            if serialized_data.is_valid():
+                instance = serialized_data.save()
+                instance.is_active = False
+                instance.save()
+                return ok(data=serialized_data.data)
+            else:
+                bad_request_msg = get_first_error_message_from_serializer_errors(serialized_data.errors,
+                                                                                 'Bad request')
+                return bad_request(data=[], message=bad_request_msg)
+        except Exception as er:
+            print(str(er))
+            return internal_server_error(data=[])
