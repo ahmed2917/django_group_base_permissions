7e2432fb8b9beee820afdc193157ccd15672c063
ahmed2917 <ahmedhassan017175@gmail.com>
2023-10-04 12:24:23 +0500

    group create url added

--- account_management/urls.py
+++ account_management/urls.py
@@ -4,4 +4,7 @@ from account_management.views import *
 urlpatterns = [
     path('signup/', SignUpViews.as_view({'post': 'create'})),
     path('signin/', SignInViews.as_view({'post': 'create'})),
+
+    path('group/create/', GroupViews.as_view({'post': 'create'})),
+
 ]
