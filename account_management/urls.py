3f6e0f659a68a7a83c2ff379fdc35dfb21dcc7ff
ahmed2917 <ahmedhassan017175@gmail.com>
2023-10-05 10:34:09 +0500

    url formatted

--- account_management/urls.py
+++ account_management/urls.py
@@ -4,6 +4,7 @@ from account_management.views import *
 urlpatterns = [
     path('signup/', SignUpViews.as_view({'post': 'create'})),
     path('signin/', SignInViews.as_view({'post': 'create'})),
+
     path('user/', UserViews.as_view({'get': 'list',
                                      'patch': 'update',
                                      "post": "create",
