fda8ca652f0d10c51a2d9abdb0882b544e1a6654
ahmed2917 <ahmedhassan017175@gmail.com>
2023-10-05 10:33:57 +0500

    user all 4 urls added

--- account_management/urls.py
+++ account_management/urls.py
@@ -4,7 +4,10 @@ from account_management.views import *
 urlpatterns = [
     path('signup/', SignUpViews.as_view({'post': 'create'})),
     path('signin/', SignInViews.as_view({'post': 'create'})),
-    path('user/', UserViews.as_view({'get': 'list', 'patch': 'update'})),
+    path('user/', UserViews.as_view({'get': 'list',
+                                     'patch': 'update',
+                                     "post": "create",
+                                     "delete": "destroy"})),
 
     path('group/create/', GroupViews.as_view({'post': 'create'})),
 
