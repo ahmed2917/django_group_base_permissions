ad3849680937d2f1899099f46bf8de7f6735a12d
ahmed2917 <ahmedhassan017175@gmail.com>
2023-10-06 10:45:22 +0500

    permission serializer added

--- account_management/serializers.py
+++ account_management/serializers.py
@@ -12,3 +12,9 @@ class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = '__all__'
+
+
+class PermissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Permission
+        fields = '__all__'
